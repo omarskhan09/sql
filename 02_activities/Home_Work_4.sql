e8000e35a90d3eafee0142091de986b5af3799a1
omarskhan09 <omarkhan09@gmail.com>
2024-09-25 21:46:25 -0400

    Home_work_4

---
+++ 02_activities/Home_Work_4.sql
@@ -0,0 +1,82 @@
+-- 1. COALESCE 
+-- SELECT product_name || ', ' || COALESCE(product_size, '') ||
+--  ' (' || COALESCE(product_qty_type, 'unit') || ')' AS product_detail
+-- FROM product;
+-- 
+-- 2. WINDOWED FUNCTIONS:
+-- 
+-- 
+-- 
+-- WITH RankedVisits AS (
+--     SELECT 
+--         customer_id, 
+--         market_date, 
+--         product_id,
+--         ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date DESC) AS visit_number,
+--         COUNT(*) OVER (PARTITION BY customer_id, product_id) AS count_product_purchase
+--     FROM 
+--         customer_purchases
+-- )
+-- SELECT 
+--     customer_id, 
+--     market_date, 
+--     product_id, 
+--     visit_number, 
+--     count_product_purchase
+-- FROM 
+--     RankedVisits
+-- WHERE 
+--     visit_number = 1;  -- Select only the most recent visit for each customer
+-- -- 
+
+--3 STRING MANIPULATION
+-- SELECT 
+--     product_name,
+--     -- Use INSTR to find the position of the hyphen and SUBSTR to capture the description after it
+--     CASE 
+--         WHEN INSTR(product_name, '-') > 0 
+--         THEN TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1)) 
+--         ELSE NULL 
+--     END AS description
+-- FROM product;
+--4 UNION
+-- WITH sales_by_date AS (
+--     SELECT market_date, 
+--         SUM(quantity * cost_to_customer_per_qty) AS total_sales
+--     FROM 
+--         customer_purchases
+--     GROUP BY  market_date
+-- ),
+-- ranked_sales AS (
+--     SELECT 
+--         market_date, 
+--         total_sales,
+--         RANK() OVER (ORDER BY total_sales DESC) AS best_day_rank,
+--         RANK() OVER (ORDER BY total_sales ASC) AS worst_day_rank
+--     FROM 
+--         sales_by_date)
+-- SELECT  market_date,  total_sales, best_day_rank, worst_day_rank
+-- FROM ranked_sales;
+-- 
+
+-- UNION to combine the highest and lowest sales days
+-- SELECT 
+--     market_date, 
+--     total_sales, 
+--     'Best Day' AS day_type
+-- FROM 
+--     ranked_sales
+-- WHERE 
+--     best_day_rank = 1
+-- 
+-- UNION ALL  -- Use UNION ALL to retain duplicates if needed
+-- 
+-- SELECT 
+--     market_date, 
+--     total_sales, 
+--     'Worst Day' AS day_type
+-- FROM 
+--     ranked_sales
+-- WHERE 
+--     worst_day_rank = 1;
+
